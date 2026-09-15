54fc3b1ea8e95bda9d9866ff203843e563bb2c91
Your Name <your-email@example.com>
2026-09-15 18:09:13 +0300

    Remove duplicate admin path in ems_project/urls.py

--- ems_project/urls.py
+++ ems_project/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('employees.urls')),  # include employee app urls
-     path('admin/', admin.site.urls),
     path('employees/', include('employees.urls')),
 ]
