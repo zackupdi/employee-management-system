98c4cf73b93e3534b6eed618d4a07d1d12274b2e
Your Name <your-email@example.com>
2026-09-19 12:18:13 +0300

    Configure STATIC_ROOT for Render

--- ems_project/settings.py
+++ ems_project/settings.py
@@ -78,6 +78,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']  # Optional: for static files in /static/
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
